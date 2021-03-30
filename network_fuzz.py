aeacba96eeef8a53d1b5a147b53733132eabbe5c
James <james.wingar@gmail.com>
2021-03-30 23:38:53 +0100

    Initial main run file

---
+++ network_fuzz.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# import from src
+from src.arguments import parse_args
+
+def main():
+    args = parse_args()
+
+    print(args)
+    # get args
+
+    # instantiate packet class
+
+    # while loop
+    # create new packet
+    # send new packet
+    # test?
+    # command line output?
+
+    # create output?
+
+if __name__ == "__main__":
+    main()
