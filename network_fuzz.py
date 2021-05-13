5dc9ff16762697d5f3f18c84364f440fdf8655bb
James <jameswingar@gmail.com>
2021-05-13 22:40:25 +0100

    Updated main file with PacketGenerator and Host class objects

--- network_fuzz.py
+++ network_fuzz.py
@@ -2,22 +2,40 @@
 
 # import from src
 from src.arguments import parse_args
+from src.hosts import Host
+from src.packet import PacketGenerator
 
 def main():
+
+     # get args
     args = parse_args()
 
     print(args)
-    # get args
 
-    # instantiate packet class
+    # Create host objects
+    target, source = Host(args['target_ip'], args['target_mac'], args['target_port']), \
+        Host(args['source_ip'], args['source_mac'], args['source_port'])
 
-    # while loop
-    # create new packet
-    # send new packet
-    # test?
-    # command line output?
+    print(target)
+    print(source)
 
-    # create output?
+    # Test hosts (online)
+
+    # Create packet generator object
+    packet_generator = PacketGenerator(args)
+
+    # Create 
+    for number in range(1):
+        # create new packet
+        packet = packet_generator.create_packet(target, source)
+
+        # send new packet
+
+        # Log output
+
+        # confirm target is online
+
+    # Output results
 
 if __name__ == "__main__":
     main()
