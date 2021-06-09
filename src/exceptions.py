681302b56829b420337fb53b5463583010de40a4
James <jameswingar@gmail.com>
2021-06-09 23:04:37 +0100

    Added additional custom exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,8 +3,20 @@
 class BaseValidationError(ValueError):
     pass
 
+#--- CLASS EXCEPTIONS----
+class InvalidHostError(BaseValidationError):
+    pass
+class InvalidPacketError(BaseValidationError):
+    pass
+class InvalidRandomiserError(BaseValidationError):
+    pass
+
 #--- HOST EXCEPTIONS----
 class HostNoIpAddressError(BaseValidationError):
+    pass
+class HostNoMacAddressError(BaseValidationError):
+    pass
+class HostNoPortAddressError(BaseValidationError):
     pass
 class HostGetLocalIpError(BaseValidationError):
     pass
@@ -13,20 +25,39 @@ class HostGetLocalMacError(BaseValidationError):
 class HostGetRemoteMacError(BaseValidationError):
     pass
 
+#--- PACKET EXCEPTIONS----
+class PacketInfoTypeError(BaseValidationError):
+    pass
+class PacketInfoKeysError(BaseValidationError):
+    pass
+class PacketInfoMissingEntriesError(BaseValidationError):
+    pass
+class PacketInfoExtraEntriesError(BaseValidationError):
+    pass
+
+#--- RANDOMISER EXCEPTIONS----
+
+
 #--- VALIDATION EXCEPTIONS----
 # IP address
+class IpAddressInvalidTypeError(BaseValidationError):
+    pass
 class IpAddressInvalidFormatError(BaseValidationError):
     pass
+class IpScopeAddressInvalidFormatError(BaseValidationError):
+    pass
 class IpAddressInvalidValueError(BaseValidationError):
+    pass
+class IpScopeAddressInvalidValueError(BaseValidationError):
     pass
 class IpAddressTooLongValueError(BaseValidationError):
     pass
 class IpAddressTooShortValueError(BaseValidationError):
     pass
-class IpScopeAddressInvalidValueError(BaseValidationError):
-    pass
 
 # Mac address
+class MacAddressInvalidTypeError(BaseValidationError):
+    pass
 class MacAddressInvalidFormatError(BaseValidationError):
     pass
 class MacAddressInvalidValueError(BaseValidationError):
@@ -37,17 +68,23 @@ class MacAddressTooShortValueError(BaseValidationError):
     pass
 
 # Port
+class PortInvalidTypeError(BaseValidationError):
+    pass
 class PortInvalidFormatError(BaseValidationError):
     pass
 class PortInvalidValueError(BaseValidationError):
     pass
 
 # Name
+class NameInvalidTypeError(BaseValidationError):
+    pass
+class NameInvalidFormatError(BaseValidationError):
+    pass
+class NameInvalidValueError(BaseValidationError):
+    pass
 class NameTooLongError(BaseValidationError):
     pass
 class NameTooShortError(BaseValidationError):
-    pass
-class NameInvalidFormatError(BaseValidationError):
     pass
 
 # Internet protocol
