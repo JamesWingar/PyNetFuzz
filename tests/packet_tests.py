5b5d95b4eeaf8668e501912aa2baf6c8d6159d63
James <jameswingar@gmail.com>
2021-06-10 21:25:02 +0100

    Added initial packet test

---
+++ tests/packet_tests.py
@@ -0,0 +1,38 @@
+import unittest
+from src import const
+
+# Module under test
+from src.packet import Packet
+
+
+# Testing the recursive sorting algorithms
+class TestArgumentParser(unittest.TestCase):
+    
+
+    def test_valid_parameters(self):
+        pass
+
+    def test_invalid_parameters(self):
+        pass
+
+    def test_add_ethernet_layer(self):
+        pass
+
+    def test_add_ip_layer(self):
+        pass
+
+    def test_add_transport_layer(self):
+        pass
+
+    def test_add_payload_layer(self):
+        pass
+
+    def test_add_all_layers(self):
+        pass
+
+    def test_send(self):
+        pass
+    
+
+if __name__ == "__main__":
+    unittest.main()
