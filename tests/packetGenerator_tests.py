adb172e5895e1d4130260ed954502ab91e898306
James <jameswingar@gmail.com>
2021-06-28 15:49:43 +0100

    Inital packetGenerator tests commit

---
+++ tests/packetGenerator_tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src import const
+import src.exceptions as ex
+
+# Module under test
+from src.packetGenerator import PacketGenerator
+
+
+# Testing the Host Class
+class TestArgumentParser(unittest.TestCase):
+
+    def test_valid_parameters(self):
+        pass
