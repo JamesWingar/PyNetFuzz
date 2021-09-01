c9978e4a5d1a79d947f07db1fedc4eb22e9a6085
James <jameswingar@gmail.com>
2021-09-01 15:02:34 +0100

    Updated and fixed packet generator

--- src/packet_generator.py
+++ src/packet_generator.py
@@ -1,6 +1,8 @@
+"""
+Contains Generator function - packet generator
+"""
 #Python library imports
-from copy import deepcopy
-
+import logging
 # Package imports
 from src.randomiser import Randomiser
 from src.packet import Packet, PacketDetails
@@ -12,39 +14,45 @@ from src.validation import (
     valid_packet_details,
 )
 
-def packet_generator(target: Host, details: PacketDetails, source: Host=Host(None, None, None), seed: int=None, max_packets: int=PACKETS_PER_SEED):
+def packet_generator(target: Host, details: PacketDetails,
+        source: Host=None, seed: int=None, max_packets: int=PACKETS_PER_SEED) -> Packet:
     """ Generator method to create randomised packets
-   
-        Parameters:
+
+    Parameters:
         target (Host): Host object containing information for packet generation
         details (PacketDetails): Object contains required details for packet generation
         source (Host): Optional Host object for packet generation
         seed (int): Value for Randomiser to create Suedo-random numbers
         max_packets (int): Value for max packets to be created from a single generator
 
-        Returns:
-        Packet: Yields a created randomised packet 
+    Returns:
+        Packet: Yields a created randomised packet
     """
-    target = target #TODO: check? (Not randomised some )
+    source =  source if source is not None else Host(None, None, None)
     details = valid_packet_details(details)
     randomiser = Randomiser(seed)
+    logging.info("Packet generator seed: %s", randomiser.seed)
 
-    random_target = deepcopy(target)
-    random_source = deepcopy(source)
-    random_details = deepcopy(details)
+    random_target = Host(None, None, None)
+    random_source = Host(None, None, None)
+    random_details = PacketDetails({
+        'trans_protocol': None,
+        'cast': None,
+        'int_protocol': None,
+        'vlan': None,
+        'headers': None})
 
-    for _ in range(max_packets):     
-        # randomise hosts   
-        random_target = randomiser.host(target, random_target) 
+    for _ in range(max_packets):
+        # randomise hosts
+        random_target = randomiser.host(target, random_target)
         random_source = randomiser.host(source, random_source)
         # randomise packet info
-        random_details = randomiser.packet_details(details, random_details, details.min_length, details.max_length) 
+        random_details = randomiser.packet_details(details, random_details)
 
         # create packet
         packet = Packet(random_target, random_source, random_details)
         packet.add_all_layers()
-        
-        # output created packet
-        print(f"Generator Packet #{_}: {packet}")
+
+        logging.debug("Generator Packet #%s: %s", _, packet)
 
         yield packet
