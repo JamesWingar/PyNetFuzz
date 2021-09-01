22713713abb2c29baaaa19ee1511b22c6a9cebf7
James <jameswingar@gmail.com>
2021-09-01 14:57:13 +0100

    Moved main loop to run.py, this means that network_fuzz.py can be used from CLI and run.py be imported to run from in a script

--- network_fuzz.py
+++ network_fuzz.py
@@ -1,65 +1,15 @@
 #!/usr/bin/env python3
 
-# import from src
+# Python library imports
 from src.arguments import parse_args
-from src.hosts import Host
-from src.packetGenerator import packet_generator
-from src.packet import PacketDetails
-from src import const
+# Package imports
+from src.run import run
+
 
 def main():
-
-     # get args
+    """ Commandline run method"""
     args = parse_args()
-
-    # Create host objects
-    target, source = Host(args.target_ip, args.target_mac, args.target_port), \
-        Host(args.source_ip, args.source_mac, args.source_port)
-
-    # Get seed from args
-    seed = args.seed
-
-    # create PacketDetails object from args
-    packet_details = PacketDetails({
-        'int_protocol': args.int_protocol,
-        'trans_protocol': args.trans_protocol,
-        'cast': args.cast,
-        'headers': args.headers,
-        'vlan': args.vlan,
-        'min_length': args.min_length,
-        'max_length': args.max_length,
-    })
-
-    # Packet count
-    packet_count = 0
-
-    while(packet_count < args.n_packets):
-
-        # Test hosts (online)
-        if not target.is_online():
-            #report offline
-            pass
-
-        for packet in packet_generator(target, packet_details, source, seed):
-
-            # send new packet
-            packet.send(args.network_interface)
-            packet_count += 1
-            
-            # Log packet
-            #TODO
-
-            # break condition
-            if args.n_packets < packet_count:
-                break
-
-        # confirm target is online
-        if not target.is_online():
-            #report offline
-            pass
-
-    # Output results
-    #TODO
+    run(args)
 
 if __name__ == "__main__":
     main()
---
+++ src/run.py
@@ -0,0 +1,76 @@
+"""
+Contains main run method and logging setup
+"""
+# Python library imports
+import logging
+import time
+# Package imports
+from src.arguments import Args
+from src.hosts import Host
+from src.packet_generator import packet_generator
+from src.packet import PacketDetails
+from src.const import LOGGING_FORMAT, LOGGING_LEVEL
+
+
+def run(args: Args) -> None:
+    """ Main run method, setups up logging, generates and sends packets. 
+    Can be imported and run from a script or run via commandline (PyNetFuzz.py)
+    
+    Parameters:
+        args (Args): An object containing all required arguments to run.
+            Contains: target_ip, source_ip, network_interface, n_packets,
+                target_mac, source_mac, target_port, source_port, int_protocol,
+                trans_protocol, cast, headers, vlan, min_length , max_length, seed
+    """
+    configure_logging()
+    logging.info("starting PynetFuzzing...")
+    target, source = Host(args.target_ip, args.target_mac, args.target_port), \
+        Host(args.source_ip, args.source_mac, args.source_port)
+    packet_details = PacketDetails({
+        'int_protocol': args.int_protocol,
+        'trans_protocol': args.trans_protocol,
+        'cast': args.cast,
+        'headers': args.headers,
+        'vlan': args.vlan,
+        'min_length': args.min_length,
+        'max_length': args.max_length,
+    })
+    logging.info("Target(%s)", target)
+    logging.info("Source(%s)", source)
+    logging.info("PacketDetails(%s)", packet_details)
+
+    packet_count, gen_count, start_time = 0, 0, time.time()
+    while(packet_count < args.n_packets):
+
+        if not target.is_online():
+            logging.error("Target is offline (%s)", target.ip)
+
+        logging.info("Starting packet generator (Sd=%s, Pkt=%s, Gen=%s)",
+            args.seed, packet_count, gen_count)
+        for packet in packet_generator(target, packet_details, source, args.seed):
+            logging.debug("%s: %s", packet_count, packet)
+            packet.send(args.network_interface)
+            packet_count += 1
+
+            if packet_count >= args.n_packets:
+                break
+
+        gen_count += 1
+        logging.info("Terminated packet generator (Sd=%s, Pkt=%s, Gen=%s)",
+            packet_generator.randomiser.seed, packet_count, gen_count)
+
+        if not target.is_online():
+            logging.error("Target is offline (%s)", target.ip)
+
+    # Output results
+    time_diff = time.time() - start_time
+    message = f"[Completed] Sent: {packet_count}, Time: {time_diff}s"
+    logging.info(message)
+    print(message)
+
+def configure_logging():
+    """ Configure logging to default file"""
+    logging.basicConfig(filename=f'logs/{int(time.time())}.log',
+        format=LOGGING_FORMAT, level=logging.INFO)
+    logger=logging.getLogger()
+    logger.setLevel(LOGGING_LEVEL.upper())
